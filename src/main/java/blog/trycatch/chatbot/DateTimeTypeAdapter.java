e7ecf2bf219a1d2a6557aac9602e27045d8c9fbe
geeordanoh <tambug@gmail.com>
2017-03-26 20:26:22 +0200

    fixed bug on DateTime conversion

---
+++ src/main/java/blog/trycatch/chatbot/DateTimeTypeAdapter.java
@@ -0,0 +1,30 @@
+package blog.trycatch.chatbot;
+
+import com.google.gson.*;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.lang.reflect.Type;
+
+
+public class DateTimeTypeAdapter implements JsonSerializer<DateTime>, JsonDeserializer<DateTime> {
+    private static final String ISO_8601_FORMAT_STRING = "yyyy-MM-dd'T'HH:mm:ss";
+
+    private DateTimeFormatter formatter;
+
+    public DateTimeTypeAdapter() {
+        formatter = DateTimeFormat.forPattern(ISO_8601_FORMAT_STRING);
+    }
+
+    @Override
+    public synchronized JsonElement serialize(DateTime date, Type type, JsonSerializationContext jsonSerializationContext) {
+        return new JsonPrimitive(formatter.print(date));
+    }
+
+    @Override
+    public synchronized DateTime deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) {
+        return formatter.parseDateTime(jsonElement.getAsString());
+
+    }
+}
--- src/main/java/blog/trycatch/chatbot/EchoController.java
+++ src/main/java/blog/trycatch/chatbot/EchoController.java
@@ -1,9 +1,10 @@
 package blog.trycatch.chatbot;
 
 
-import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import io.swagger.client.ApiException;
 import io.swagger.client.model.Activity;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +20,21 @@ public class EchoController {
     @RequestMapping(value = "/messages", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE})
     @ResponseBody
     public ResponseEntity processMessage(@RequestBody String requestString) throws ApiException {
-        Gson gson = new Gson();
-        Activity activity = gson.fromJson(requestString, Activity.class);
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(DateTime.class, new DateTimeTypeAdapter()).create();
+        Activity activity = gsonBuilder.create().fromJson(requestString, Activity.class);
         if (activity.getType().equals("message")) {
             messageProcessorComponent.enqueueMessage(activity);
         } else {
             System.out.println("I don't understand messages of type: " + activity.getType());
         }
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(value = "/messages", method = RequestMethod.GET, consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    @ResponseBody
+    public ResponseEntity processMessage() throws ApiException {
+        System.out.println("GET");
         return ResponseEntity.ok().build();
     }
 }
