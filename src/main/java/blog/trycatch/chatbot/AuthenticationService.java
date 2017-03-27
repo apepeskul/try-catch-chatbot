77f68be5e34c19a37576d38a0c533d603295ecb9
geeordanoh <tambug@gmail.com>
2017-03-27 21:33:16 +0200

    Refactoring

--- src/main/java/blog/trycatch/chatbot/AuthenticationService.java
+++ src/main/java/blog/trycatch/chatbot/AuthenticationService.java
@@ -4,10 +4,6 @@ import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -15,9 +11,6 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Service
 public class AuthenticationService {
